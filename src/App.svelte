abc36ccac8c5bf95d06086075eeffeb04cb03b25
adjaythakoerdien <a.thakoerdien@gmail.com>
2022-03-08 13:47:19 +0100

    Form added for adding Meetups

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,14 @@
     import Header from './UI/Header.svelte';
     import MeetupGrid from "./MeetUps/MeetupGrid.svelte";
 
-    const meetups = [
+    let title = '';
+    let subtitle = '';
+    let address = '';
+    let email = '';
+    let description = '';
+    let imageUrl = '';
+
+    let meetups = [
         {
             id: 'm1',
             title: 'Coding bootcamp',
@@ -22,6 +29,23 @@
             contactEmail: 'code@test.com'
         }
     ]
+
+    function addMeetup() {
+        const newMeetup = {
+            id: Math.random().toString(),
+            title: title,
+            subtitle: subtitle,
+            description: description,
+            imageUrl: imageUrl,
+            contactEmail: email,
+            address: address
+        };
+
+        meetups = [
+            newMeetup,
+            ...meetups
+        ];
+    }
 </script>
 
 <style>
@@ -32,5 +56,33 @@
 
 <Header />
 <main>
+    <form on:submit|preventDefault={addMeetup}>
+        <div class="form-control">
+            <label for="title">Titel</label>
+            <input type="text" id="title" bind:value={title}>
+        </div>
+        <div class="form-control">
+            <label for="subtitle">Subtitle</label>
+            <input type="text" id="subtitle" bind:value={subtitle}>
+        </div>
+        <div class="form-control">
+            <label for="address">Address</label>
+            <input type="text" id="address" bind:value={address}>
+        </div>
+        <div class="form-control">
+            <label for="imageUrl">Image URL</label>
+            <input type="text" id="imageUrl" bind:value={imageUrl}>
+        </div>
+        <div class="form-control">
+            <label for="email">Email</label>
+            <input type="email" id="email" bind:value={email}>
+        </div>
+        <div class="form-control">
+            <label for="description">Description</label>
+            <textarea rows="3" id="description" bind:value={description}></textarea>
+        </div>
+        <button type="submit">Submit</button>
+
+    </form>
     <MeetupGrid {meetups}/>
 </main>
